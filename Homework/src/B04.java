3bca4c56a46b29405d0db3739dbf000b61b309b2
jihunhong <redgee49@gmail.com>
2016-10-07 18:06:29 +0900

    B04수정

--- Homework/src/B04.java
+++ Homework/src/B04.java
@@ -1,43 +1,41 @@
-import.java.util.*
+import java.util.*;
 
       class B04{
-      
+      double so = 0;
+      double don = 0;
        public static void main(String[] args)
        {
-        new B04().start()
+        new B04().start();
        }
        
        void start()
        {
-       Scanner s = new Scanner(System.in)
-       System.out.println("연봉을 원단위로 입력하시오.")       
-       int don = s.nextInt();
+       Scanner s = new Scanner(System.in);
+       System.out.print("연봉을 원단위로 입력하시오.");       
+        this.don = s.nextInt();
        
        print();
        }
        
        void print()
        {
-       if(don<1000000000)
+       if(don<10000000)
        {
-       int so = don * 0.095
-       System.out.println("연봉 금액에 대한 소득세는"+so+ 입니다.);
+        so = don * 0.095;
        }
-       if(don<40000000)
+       else if(don<40000000)
        {
-              int so = don * 0.19
-       System.out.println("연봉 금액에 대한 소득세는"+so+ 입니다.);
+               so = don * 0.19;
        }
-       if(don<80000000)
+       else if(don<80000000)
        {
-                    int so = don * 0.28
-       System.out.println("연봉 금액에 대한 소득세는"+so+ 입니다.);       
+                   so = don * 0.28;    
        }
-       if(don>80000000)
+       else if(don>80000000)
        {
-                int so = don * 0.37
-       System.out.println("연봉 금액에 대한 소득세는"+so+ 입니다.); 
+                 so = don * 0.37;
        }
+       System.out.println("연봉 금액에 대한 소득세는"+so+ "입니다."); 
       }
  }
            
